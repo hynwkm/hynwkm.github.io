e7a74f6bcc6d02a59d941237790bf8bd189dbc89
haagen24 <40901301+haagen24@users.noreply.github.com>
2024-08-14 09:53:27 -0400

    fix package json

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/hynwkm.github.io",
+    base: "/hynwkm.github.io/",
     plugins: [react()],
     css: {
         postcss: {
